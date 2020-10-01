336dd604a4e329c20db12622bdbbfe79372f9795
Daniel Kim <daniel_kim1@zohomail.com>
2020-09-30 17:06:28 -0700

    forgot to commit project

--- index.js
+++ index.js
@@ -7,12 +7,20 @@ const breakfastBurrito = {name: "Breakfast Burrito", price: 16, category:"Breakf
 /* Task 1a: write a function to return more menu items with the same format as the items above. */
 
 function createMenuItem(name, price, category){
-    /* Code here */
+  const newMenuItem = {};
+  newMenuItem.name = name;
+  newMenuItem.price = price;
+  newMenuItem.category = category;
+  return newMenuItem;
 }
 
 /* Task 1b: use your function to create 3 more menu items. You may add any items to the menu that you'd like */
 
+const omelette = createMenuItem('Omelette', 5, 'Breakfast');
+const coffee = createMenuItem('Coffee', 3, 'Drink');
+const countryFriedSteak = createmenuItem('Country Fried Steak', 15, 'Breakfast')
 
+console.log(omelette, Coffee, countryFriedSteak);
 
 /* Task 2: You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to your burger object that automatically calculates price given a string as a parameter. 
 
@@ -23,8 +31,19 @@ Your method should accept:
 and should return a number. 
 
 For example, burger.discount("teacher") would return 13.5 and burger.discount("public") would return 16.2*/
+burger.discount = function (type) {
+  if (type === "teacher") {
+    return this.price * .75;
+  }
+  else if (type === "student") {
+    return this.price * .90;
+  }
+  else {
+    return this.price;
+  }
+}
 
-
+console.log(burger.discount("teacher"));
 
 ///////////////Reviews (MVP)///////////////////
 
@@ -40,11 +59,23 @@ const reviews = [{name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and
 
 /* Task 3: Console.log just Julius' feedback */
 
+console.log(reviews[5].feedback);
 
 /* Task 4: Add a new rating with your (fictitious) opinions of the restaurant in the same format as the reviews above. */
 
+reviews.push({
+  name: "Luke",
+  rating: 5,
+  feedback: "Awesome food"
+});
 
-/* Task 5: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
+console.log(reviews[8].feedback)
+
+/* Task 5: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays" */
+
+reviews[7].feedback = "this place is chill with really cool people, great for getting work done on weekdays";
+
+console.log(reviews[7].feedback)
 
 /*  Task 6: Write a function to return a review based on the index of the review in the array.
 
@@ -58,10 +89,11 @@ and should return a string in the format `{name} gave the restaurant a {rating},
  * For example, if getReviewByIndex is invoked with reviews and the number 0
  * it will return `Daniela gave the restaurant a 5 star review and their feedback was: Beautiful atmosphere and wonderful vegan options!`
 */
-function getReviewByIndex(reviews, index) {
-    /* code here */
+function getReviewByIndex(revCollection, index) {
+    return `${revCollection[index].name} gave the restaurant a ${revCollection[index].rating}, and their feedback was: ${revCollection[index].feddback}`;
   }
   
+  console.log(getReviewByIndex(reviews, 8))
 
 /* Task 7: Write a function to get information about the most recent review called `getLastReview`
 
@@ -72,9 +104,12 @@ and should return a string in the format `name} gave the restaurant a {rating}, 
 
 For example, if getLastReview is invoked passing the reviews array it will return `Reyna gave the restaurant a 3.5 star review and their feedback was: "this place is chill with really cool people, great for getting work done on weekdays"`.
 */
-function getLastReview(/* code here */) {
-    /* code here */
+function getLastReview(revCollection) {
+    const index = revCollection.length - 1;
+    return `${revCollection[index].name} gave the restaurant a ${revCollection[index].rating}, and their feedback was: ${revCollection[index].feedback}`; 
   } 
+
+  console.log(getLastReview(reviews));
 
 
 ///////////////🍔☕️🍽 STRETCH🍔☕️🍽////////////////////
